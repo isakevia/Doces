1b9f76d9a04f53be47dcb133e9e2d0fdb2ba1d07
isakevia <isabellisilva506@gmail.com>
2023-08-30 15:03:46 -0300

    comecei arrumar

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StatusBar, SafeAreaView } from 'react-native';
+import Doces from './src/pages/Doces';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar/>
+      <Doces/>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Doces/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
+
+import logo from '../../../assets/logo.png';
+
+const width = Dimensions.get('screen').width;
+
+export default function Doces () {
+    return <>
+    <Image source={logo} style={estilos.logo}/>
+    
+    </>
+}
+
+const estilos = StyleSheet.create({
+    logo: {
+        width: "100%",
+      height: 578 / 768 * width
+  },
+})
