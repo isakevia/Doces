7d7d3093f69cddb3e20d8dca1758de94d5437309
isakevia <isabellisilva506@gmail.com>
2023-09-27 15:06:08 -0300

    adicionei mais elementos e componetizei

---
+++ src/pages/Doces/components/Detalhe.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
+
+import bala from "../../../../assets/bala.jpg";
+import uva from "../../../../assets/uva.jpg";
+import morango from "../../../../assets/morango.jpg"
+import maca from "../../../../assets/maca.jpg"
+
+export default function Detalhe () {
+    return<>
+
+<Text style={estilos.nomeDoceria}>Doces</Text>
+
+<View style={estilos.bala}>
+  <Image style={estilos.imagem} source={bala} />
+  <Text style={estilos.nome}>Bala Baiana</Text>
+</View>
+
+<Text style={estilos.descricao}>Beijinho passado no caramelo.</Text>
+
+<Text style={estilos.preco}>R$ 10,00 {'\n'}</Text>
+
+
+<View style={estilos.uva}>
+  <Image style={estilos.imagem} source={uva} />
+  <Text style={estilos.nome}>Surpresa de Uva</Text>
+</View>
+
+<Text style={estilos.descricao}>Brigadeiro de leite ninho recheado com uva.</Text>
+
+<Text style={estilos.preco}>R$ 10,00</Text>
+
+<View style={estilos.morango}>
+  <Image style={estilos.imagem} source={morango} />
+  <Text style={estilos.nome}>Coxinha de morango</Text>
+</View>
+
+<Text style={estilos.descricao}>Brigadeiro ao leite recheado com morango inteiro.</Text>
+
+<Text style={estilos.preco}>R$ 4,50</Text>
+
+<View style={estilos.maca}>
+  <Image style={estilos.imagem} source={maca} />
+  <Text style={estilos.nome}>Maçã do amor</Text>
+</View>
+
+<Text style={estilos.descricao}>Maçã com uma calda de caramelo.</Text>
+
+<Text style={estilos.preco}>R$ 4,50</Text>
+    </>
+}
+
+const estilos = StyleSheet.create({
+   
+    imagemDoceria: {
+      width: 32,
+      height: 32,
+    },
+  
+    nomeDoceria: {
+      fontSize: 26,
+      lineHeight: 42,
+      color: "#000000",
+    },
+  
+    descricao: {
+      color: "#836FFF",
+      fontStyle: "italic",
+    },
+  
+    nome: {
+      fontSize: 16,
+      lineHeight: 50,
+      marginLeft: 12,
+      fontWeight: "bold"
+    },
+  
+    imagem: {
+      width: 62,
+      height: 62,
+      borderRadius: 10,
+    },
+  
+    bala: {
+      flexDirection: "row",
+      paddingVertical: 12,
+    },
+  
+    uva: {
+      flexDirection: "row",
+      paddingVertical: 12,
+    },
+  
+    morango: {
+      flexDirection: "row",
+      paddingVertical: 12,
+    },
+
+    maca: {
+        flexDirection: "row",
+        paddingVertical: 12,
+      },
+  
+    preco: {
+      fontSize: 26,
+      lineHeight: 42,
+      color: "#836FFF",
+      marginTop: 8,
+    }
+});
